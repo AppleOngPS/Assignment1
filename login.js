0c24471fdea7773666a68f855356bfaaf4ef868e
AppleOngPS <ongapple1@gmail.com>
2023-11-23 23:05:32 +0800

    update

--- login.js
+++ login.js
@@ -1,16 +1,23 @@
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
-
-loginButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  const username = loginForm.username.value;
-  const password = loginForm.password.value;
-
-  if (username === "user" && password === "web_dev") {
-    alert("You have successfully logged in.");
-    location.reload();
+function login() {
+  var username = document.getElementById("email").value;
+  var password = document.getElementById("pwd").value;
+  // filter function is to make sure it is the correct format.. eg example@gmail.com
+  var filter =
+    /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  // if user did not fill up this part it will pop the alert
+  if (username == "") {
+    alert("please enter email.");
+    // if user did not fill up this part it will pop the alert
+  } else if (password == "") {
+    alert("enter the password");
+    // if the email is wrong format it will pop out invaild email,please try again
+  } else if (!filter.test(username)) {
+    alert("invaild email,please try again!.");
+    // the password need to be 6 charater/digit length long
+  } else if (password.length < 6 || password.length > 6) {
+    alert("Password min and max length is 6.");
   } else {
-    loginErrorMsg.style.opacity = 1;
+    // if all is true , it will pop out login succesfull
+    alert("Login Successfull!");
   }
-});
+}
