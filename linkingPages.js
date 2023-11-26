733e99d5d95cf763ffa0ac19abe6b67c2518faaf
AppleOngPS <ongapple1@gmail.com>
2023-11-26 10:33:36 +0800

    update

---
+++ linkingPages.js
@@ -0,0 +1,21 @@
+function home() {
+  window.location.href = "index.html";
+}
+function about() {
+  window.location.href = "about.html";
+}
+function enrollment() {
+  window.location.href = "enrollment.html";
+}
+function exhibition() {
+  window.location.href = "exhibition.html";
+}
+function publication() {
+  window.location.href = "publication.html";
+}
+function news() {
+  window.location.href = "news.html";
+}
+function login() {
+  window.location.href = "login.html";
+}
